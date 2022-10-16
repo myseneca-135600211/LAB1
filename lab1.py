3bfbd711952cb04d91ad047b82e9c808e4f02774
Gitesh <113646387+myseneca-135600211@users.noreply.github.com>
2022-10-16 16:17:42 -0400

    Add files via upload

---
+++ lab1.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+
+def age_caculator():
+    year_of_birth = input("Enter your birth year: ")
+    age = 2022-int(year_of_birth)
+    return f"You are {age} years old"
+
+print(age_caculator())
